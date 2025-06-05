ec11ff9224cb244a63c13aef05a223038b97aa7d
jace6789 <jaceviolette@gmail.com>
2025-06-04 20:03:22 -0700

    Create ManiaModKey16.cs

---
+++ osu.Game.Rulesets.Mania/Mods/ManiaModKey16.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Mania.Mods
+{
+    public class ManiaModKey16 : ManiaKeyMod
+    {
+        public override int KeyCount => 16;
+        public override string Name => "Sixteen Keys";
+        public override string Acronym => "16K";
+        public override LocalisableString Description => @"Play with sixteen keys.";
+        public override bool Ranked => false;
+    }
+}
