09a2bc0f2b945728b0a6ab80441d82150c5c8e7a
jace6789 <jaceviolette@gmail.com>
2025-06-04 20:07:51 -0700

    Create ManiaModKey20.cs

---
+++ osu.Game.Rulesets.Mania/Mods/ManiaModKey20.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Mania.Mods
+{
+    public class ManiaModKey20 : ManiaKeyMod
+    {
+        public override int KeyCount => 20;
+        public override string Name => "Twenty Keys";
+        public override string Acronym => "20K";
+        public override LocalisableString Description => @"Play with twenty keys. Yeah, that's right, TWENTY. KEYS.";
+        public override bool Ranked => false;
+    }
+}
