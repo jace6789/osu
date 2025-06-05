0516fd3b1ece96237f78223953690b072b315177
jace6789 <jaceviolette@gmail.com>
2025-06-04 20:06:46 -0700

    Create ManiaModKey19.cs

---
+++ osu.Game.Rulesets.Mania/Mods/ManiaModKey19.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Mania.Mods
+{
+    public class ManiaModKey19 : ManiaKeyMod
+    {
+        public override int KeyCount => 19;
+        public override string Name => "Nineteen Keys";
+        public override string Acronym => "19K";
+        public override LocalisableString Description => @"Play with nineteen keys.";
+        public override bool Ranked => false;
+    }
+}
